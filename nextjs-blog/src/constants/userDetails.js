8c6f21a493e4868e99921f8f5b014662c213d9ed
ErinSY <seoyul0203@gmail.com>
2022-11-04 04:07:44 +0900

    API 라우팅

---
+++ nextjs-blog/src/constants/userDetails.js
@@ -0,0 +1,7 @@
+// 고정값을 저장하는곳
+
+export const userDetail = {
+  name: 'Erin',
+  age: 31,
+  height: '2m',
+};
--- nextjs-blog/src/pages/[username]/[info].js
+++ nextjs-blog/src/pages/[username]/[info].js
@@ -1,16 +1,31 @@
 import Layout from 'components/Layout';
 import SubLayout from 'components/SubLayout';
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 export default function UserNameInfo() {
+  const [name, setName] = useState('?');
   const router = useRouter();
-  const { username, info } = router.query;
+  const { username, info, uid } = router.query;
+  // useEffect(() => {
+  //   fetch('/api/user')
+  //     .then((res) => res.json())
+  //     .then((data) => setName(data.name));
+  // }, []);
+
+  useEffect(() => {
+    uid &&
+      fetch(`/api/user-info/${uid}`)
+        .then((res) => res.json())
+        .then((data) => setName(data.name));
+  }, [uid]);
   return (
     <>
       <main>
         <h1 className='UserNameInfo'>
           {username} {info}
         </h1>
+        <h1 className='Myname'>Name:{name}</h1>
       </main>
     </>
   );
