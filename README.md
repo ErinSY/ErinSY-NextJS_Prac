9c2afbf3d72e326ddb2a1c09ec8a05155a1b0da1
ErinSY <seoyul0203@gmail.com>
2022-11-04 01:20:39 +0900

    절대경로춫가

---
+++ README.md
@@ -0,0 +1,11 @@
+## 절대경로 설정
+
+### `jsconfig.json`파일에 아래코드 추가
+
+```js
+{
+  "compilerOptions": {
+    "baseUrl": "src"
+  },
+}
+```
--- nextjs-blog/src/components/Layout.js
+++ nextjs-blog/src/components/Layout.js
@@ -21,6 +21,23 @@ export default function Layout({ children }) {
       </footer>
 
       <style jsx>{`
+       
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -148,21 +165,6 @@ export default function Layout({ children }) {
             width: 100%;
             flex-direction: column;
           }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
         }
       `}</style>
     </div>
