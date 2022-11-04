dada727c9fb668b31e20ee2059406ff112b89563
ErinSY <seoyul0203@gmail.com>
2022-11-04 17:33:12 +0900

    ADD: 공통컴포넌트 + css module

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,70 +1,34 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className="container">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <h1 className="title">
-          Read this article{' '}
-          <Link className="href" href="/posts/first-post">
-            첫번째 글
-          </Link>
-        </h1>
-        <h1 className="title">
-          Read this article <a href="/posts/first-post">첫번째 글</a>
-        </h1>
+        <main>
+          <h1 className="title">
+            Read this article{' '}
+            <Link className="href" href="/posts/first-post">
+              첫번째 글
+            </Link>
+          </h1>
+          {/* <img src="/Images/profile.jpeg" alt="Erin" /> */}
+          <Image
+            src="/Images/profile.jpeg"
+            alt="Erin"
+            width={140}
+            height={140}
+          />
+        </main>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
+        {/* <style jsx>{`
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -104,7 +68,7 @@ export default function Home() {
 
         .href {
           color: pink;
-          text-decoration: none;
+          text-decoration: ;
         }
         a {
           color: inherit;
@@ -212,7 +176,8 @@ export default function Home() {
         * {
           box-sizing: border-box;
         }
-      `}</style>
-    </div>
+      `}</style> */}
+      </div>
+    </Layout>
   )
 }
